5a7a57b19a295d4b72f5a8f4b7d8d440d08ade9b
Aaditya Shah <ashah203@uottawa.ca>
2023-01-22 03:13:39 -0500

    Header and footer setup

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <div class="navbar is-dark">
+      <!--LOGO-->
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          Insert a logo here
+        </a>
+      </div>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+}
